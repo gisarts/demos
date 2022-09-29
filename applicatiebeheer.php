f882d335c9ae96b91a572039426f67c5e434538e
JucksAble <joey.haerkens@student.gildeopleidingen.nl>
2022-09-29 12:54:22 +0200

    Finished additional pages

--- applicatiebeheer.php
+++ applicatiebeheer.php
@@ -6,20 +6,20 @@
             <div class="col-lg-5 align-self-center">
                 <h4 class="text-gold m-0">USERGEBONDEN</h4>
                 <h2>Voeg zelfstandig gebruikers toe met <b>CO</b>OK</h2>
-                <p class="w-75">Als applicatiebeheerder is het mogelijk om zelfstandig gebruikers toe te voegen. Aan deze gebruikers kunnen functionaliteiten worden toegewezen zodat iedereen over zijn eigen informatie beschikt. Alles voldoet volledig aan de AVG richtlijnen. Daarnaast is het mogelijk om inzicht te krijgen in wat de gebruikers doen. Hoe vaak en wanneer iemand ingelogd heeft, wie de GIS Viewer gebruikt en hoe ze deze gebruiken. Zo kan er altijd gemonitord worden of de user voldoende gebruik maakt van de COOK zodat er nooit teveel betaald wordt.</p>
+                <p class="w-75 r-text">Als applicatiebeheerder is het mogelijk om zelfstandig gebruikers toe te voegen. Aan deze gebruikers kunnen functionaliteiten worden toegewezen zodat iedereen over zijn eigen informatie beschikt. Alles voldoet volledig aan de AVG richtlijnen. Daarnaast is het mogelijk om inzicht te krijgen in wat de gebruikers doen. Hoe vaak en wanneer iemand ingelogd heeft, wie de GIS Viewer gebruikt en hoe ze deze gebruiken. Zo kan er altijd gemonitord worden of de user voldoende gebruik maakt van de COOK zodat er nooit teveel betaald wordt.</p>
             </div>
             <div class="col-lg-7 py-5">
-                <img src="./img/app1.png" class="img-fluid" style="height: 600px;" alt="app1">
+                <img src="./img/app1.png" class="img-fluid" alt="app1">
             </div>
         </div>
         <div class="row">
             <div class="col-lg-7 py-5">
                 <img src="./img/app2.png" class="img-fluid" alt="app1">
             </div>
-            <div class="col-lg-4 offset-md-1 align-self-center">
-                <h4 class="text-gold m-0">USERGEBONDEN</h4>
-                <h2>Voeg zelfstandig gebruikers toe met <b>CO</b>OK</h2>
-                <p class="w-75">Als applicatiebeheerder is het mogelijk om zelfstandig gebruikers toe te voegen. Aan deze gebruikers kunnen functionaliteiten worden toegewezen zodat iedereen over zijn eigen informatie beschikt. Alles voldoet volledig aan de AVG richtlijnen. Daarnaast is het mogelijk om inzicht te krijgen in wat de gebruikers doen. Hoe vaak en wanneer iemand ingelogd heeft, wie de GIS Viewer gebruikt en hoe ze deze gebruiken. Zo kan er altijd gemonitord worden of de user voldoende gebruik maakt van de COOK zodat er nooit teveel betaald wordt.</p>
+            <div class="col-lg align-self-center">
+                <h4 class="text-gold m-0">IMPORT EXPORT BESTANDEN</h4>
+                <h2>Voeg zelfstandig bestanden toe van KML tot JSON</h2>
+                <p class="w-75 r-text">In de Geo wereld wordt met veel verschillende bestanden gewerkt, Deze kan de klant zelf toevoegen aan de GIS Viewer van COOK. Andersom is het ook mogelijk om gegevens te exporteren naar Shape en Excel bestanden. Daarnaast is het ook mogelijk om KML en GeoJSon bestanden toe te voegen. Sleep de bestanden in het kaartbeeld en COOK geeft de informatie weer. PDOK heeft een groot aantal datasets downloads beschikbaar. Ook deze kunnen moeiteloos in de COOK GIS Viewer worden toegevoegd.</p>
             </div>
         </div>
     </div>
--- functies.php
+++ functies.php
@@ -7,10 +7,10 @@
             <div class="col">
                 <div class="card mb-5 rounded-4 f-card">
                     <div class="card-body">
-                        <img src="./img/bestanden.png" alt="">
-                        <h4 class="text-gold"><b>BESTANDEN</b> IMPORT & EXPORT</h4>
+                        <img src="./img/functie2.png" alt="">
+                        <h4 class="text-gold"><b>FILTEREN</b> OP VRAAG</h4>
                         <p class="fs-6 text-gold">
-                            Importeer en exporteer verschillende soorten bestanden met COOK van Json tot KML
+                            De COOK Gis Viewer bevat enorm veel informatie. Met de functie is het mogelijk om informatie te filteren. Vink de filters makkelijk uit/aan. 
                         </p>
                     </div>
                 </div>
@@ -19,10 +19,10 @@
             <div class="col">
                 <div class="card mb-5 rounded-4 f-card">
                     <div class="card-body">
-                        <img src="./img/bestanden.png" alt="">
-                        <h4 class="text-gold"><b>BESTANDEN</b> IMPORT & EXPORT</h4>
+                        <img src="./img/functie3.png" alt="">
+                        <h4 class="text-gold">LEGENDA <b>AANPASSEN</b></h4>
                         <p class="fs-6 text-gold">
-                            Importeer en exporteer verschillende soorten bestanden met COOK van Json tot KML
+                            De legenda van de COOK GIS Viewer kan zelf aangepast en ingericht worden. Markeer favoriete locaties, filter informatie en maak selecties. 
                         </p>
                     </div>
                 </div>
@@ -31,10 +31,10 @@
             <div class="col">
                 <div class="card mb-5 rounded-4 f-card">
                     <div class="card-body">
-                        <img src="./img/bestanden.png" alt="">
-                        <h4 class="text-gold"><b>BESTANDEN</b> IMPORT & EXPORT</h4>
+                        <img src="./img/functie4.png" alt="">
+                        <h4 class="text-gold"><b>3D</b> VIEWER</h4>
                         <p class="fs-6 text-gold">
-                            Importeer en exporteer verschillende soorten bestanden met COOK van Json tot KML
+                            Bekijk topografie of huizen op een realistische wijze. Met landmeetkundige apparatuur en drones is het 3D model in de GIS Viewer op de cm nauwkeurig.
                         </p>
                     </div>
                 </div>
@@ -43,10 +43,10 @@
             <div class="col">
                 <div class="card mb-5 rounded-4 f-card">
                     <div class="card-body">
-                        <img src="./img/bestanden.png" alt="">
-                        <h4 class="text-gold"><b>BESTANDEN</b> IMPORT & EXPORT</h4>
+                        <img src="./img/functie1.png" alt="">
+                        <h4 class="text-gold"><b>CAD</b> TEKENINGEN</h4>
                         <p class="fs-6 text-gold">
-                            Importeer en exporteer verschillende soorten bestanden met COOK van Json tot KML
+                            De technische CAD tekeningen worden moeiteloos in COOK geladen. Handig voor het raadplegen van een project of een handige fasering op kaart
                         </p>
                     </div>
                 </div>
@@ -55,10 +55,10 @@
             <div class="col">
                 <div class="card mb-5 rounded-4 f-card">
                     <div class="card-body">
-                        <img src="./img/bestanden.png" alt="">
-                        <h4 class="text-gold"><b>BESTANDEN</b> IMPORT & EXPORT</h4>
+                        <img src="./img/functie5.png" alt="">
+                        <h4 class="text-gold"><b>ROUTE</b> PLANNING</h4>
                         <p class="fs-6 text-gold">
-                            Importeer en exporteer verschillende soorten bestanden met COOK van Json tot KML
+                            De GIS Viewer van COOK bevat een routeplanner. Handig om digitale codes die in het veld aangebracht zijn terug te vinden. Of gewoon de route op te vragen.
                         </p>
                     </div>
                 </div>
@@ -67,10 +67,10 @@
             <div class="col">
                 <div class="card mb-5 rounded-4 f-card">
                     <div class="card-body">
-                        <img src="./img/bestanden.png" alt="">
-                        <h4 class="text-gold"><b>BESTANDEN</b> IMPORT & EXPORT</h4>
+                        <img src="./img/functie6.png" alt="">
+                        <h4 class="text-gold"><b>ACTUELE</b> SATELLIETBEELDEN</h4>
                         <p class="fs-6 text-gold">
-                            Importeer en exporteer verschillende soorten bestanden met COOK van Json tot KML
+                            Naast luchtfoto's wordt er ook gebruikt gemaakt van satellietbeelden in de GIS Viewer van COOK. Gemiddeld zes keer per jaar van hoge resolutie.
                         </p>
                     </div>
                 </div>
@@ -82,9 +82,9 @@
 
 <div class="row align-items-center" style="background-color: #A98112;">
     <div class="col-lg-7" style="background-color: #D4BE87;">
-        <div class="container-sm" style="padding: 5rem;">
+        <div class="container-sm p-5 m-pad">
             <h2 class="text-gold"><b>TIJDREIZEN</b> MET COOK</h2>
-            <p class="w-75">Wij zijn bij GISArts verzot op kaarten, zo is het mogelijk om bij ons te tijdreizen. De ontwikkeling van de historische Topografische Militaire kaart van 1850 of de bonnenbladen van 1900 en dat gecombineerd met de moderne techniek van nu. Prachtig om historie te combineren met eigen data. Dit is allemaal mogelijk in de Geo Viewer van COOK. U kunt de historie en het verloop volgen van ieder interessant gebied. Enorm handig voor Bosbeheer, enorm interessant voor iedereen. Probeer het zelf eens uit in de demo!</p>
+            <p class="w-75 r-text">Wij zijn bij GISArts verzot op kaarten, zo is het mogelijk om bij ons te tijdreizen. De ontwikkeling van de historische Topografische Militaire kaart van 1850 of de bonnenbladen van 1900 en dat gecombineerd met de moderne techniek van nu. Prachtig om historie te combineren met eigen data. Dit is allemaal mogelijk in de Geo Viewer van COOK. U kunt de historie en het verloop volgen van ieder interessant gebied. Enorm handig voor Bosbeheer, enorm interessant voor iedereen. Probeer het zelf eens uit in de demo!</p>
         </div>
     </div>
     <div class="col-lg-5 py-5">
@@ -101,18 +101,18 @@
         </div>
     </div>
     <div class="col-lg-7">
-        <div class="container-sm px-5">
+        <div class="container-sm p-5">
             <h2 class="text-gold">FUNCTIES VAN DE <b>QUERYBUILDER</b></h2>
-            <p class="w-75">Met dit gereedschap kan je zelfstandig kaarten maken. Je stelt als het ware een vraag aan de kaart door een gebied te tekenen, door vragen te stellen, bijvoorbeeld: 'Laat mij alle gebouwen die na het jaar 2000 gebouwd zijn zien'. De gebouwen die voldoen aan de gestelde filters lichten op. Deze video laat zien hoe het werkt.</p>
+            <p class="w-75 r-text">Met dit gereedschap kan je zelfstandig kaarten maken. Je stelt als het ware een vraag aan de kaart door een gebied te tekenen, door vragen te stellen, bijvoorbeeld: 'Laat mij alle gebouwen die na het jaar 2000 gebouwd zijn zien'. De gebouwen die voldoen aan de gestelde filters lichten op. Deze video laat zien hoe het werkt.</p>
         </div>
     </div>
 </div>
 
 <div class="row align-items-center" style="background-color: #A98112;">
     <div class="col-lg-7" style="background-color: #D4BE87;">
-        <div class="container-sm" style="padding: 5rem;">
+        <div class="container-sm p-5 m-pad">
             <h2 class="text-gold"><b>360°</b> FOTO'S</h2>
-            <p class="w-75">De foto's (streetview) worden bijna overal gemaakt door de auto's van Cyclomedia. Toch kunnen deze auto's niet op alle plekken komen. Daarom zit er in de GIS Viewer van COOK een 360° view. Plekken waar weinig mensen komen, geen wegen lopen of niet bereikbaar zijn. Voor onze klanten is het mogelijk om de beelden zelf aan COOK toe te voegen of te laten maken door GISArts. Bekijk de video om te zien hoe het werkt.</p>
+            <p class="w-75 r-text">De foto's (streetview) worden bijna overal gemaakt door de auto's van Cyclomedia. Toch kunnen deze auto's niet op alle plekken komen. Daarom zit er in de GIS Viewer van COOK een 360° view. Plekken waar weinig mensen komen, geen wegen lopen of niet bereikbaar zijn. Voor onze klanten is het mogelijk om de beelden zelf aan COOK toe te voegen of te laten maken door GISArts. Bekijk de video om te zien hoe het werkt.</p>
         </div>
     </div>
     <div class="col-lg-5 py-5">
@@ -129,9 +129,9 @@
         </div>
     </div>
     <div class="col-lg-7">
-        <div class="container-sm px-5">
+        <div class="container-sm p-5">
             <h2 class="text-gold">BIRDSEYE <b>WEERGAVE</b></h2>
-            <p class="w-75">Deze beelden zijn onder een hoe van 45° opgenomen en bezitten veel detail informatie. De beelden kunnen in de GIS Viewer van COOK opgeroepen worden waaruit ze van 4 kanten te bekijken zijn. De beelden zijn van hoge kwaliteit en worden gebruik bij planning, bouw/infrastructuur, groenvoorziening en veel meer.</p>
+            <p class="w-75 r-text">Deze beelden zijn onder een hoe van 45° opgenomen en bezitten veel detail informatie. De beelden kunnen in de GIS Viewer van COOK opgeroepen worden waaruit ze van 4 kanten te bekijken zijn. De beelden zijn van hoge kwaliteit en worden gebruik bij planning, bouw/infrastructuur, groenvoorziening en veel meer.</p>
         </div>
     </div>
 </div>

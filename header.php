977e333ac345f9cc2400aee2583001def4b0c860
JucksAble <joey.haerkens@student.gildeopleidingen.nl>
2022-10-17 13:08:13 +0200

    Add Meta tags + Favicon

--- header.php
+++ header.php
@@ -5,23 +5,34 @@ $page = $_GET['page'];
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="nl-NL">
     <head>
-        <!-- <base href="/"> -->
-        <title>Cook Demo</title>
+        <title>Cook Demo | Gis Viewer</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="robots" content="index,follow">
+        <meta name="description" content="Probeer hier de GIS Viewer van Cook. Op deze pagina kunt u de demo GIS viewer van Cook bekijken.">
+
+        <meta property="og:title" content="Cook Demo | Gis Viewer" />
+        <meta property="og:description" content="Probeer hier de GIS Viewer van Cook. Op deze pagina kunt u de demo GIS viewer van Cook bekijken." />
+        <meta property="og:image" content="https://demo.gisarts.nl/img/logo_cook.webp" />
+        <meta property="og:site_name" content="Cook Demo" />
+        <meta property="og:url" content="https://demo.gisarts.nl/" />
+
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="subject" content="Serve The World">
-        <meta name="description" content="Probeer hier de GIS Viewer van Cook. Op deze pagina kunt u de demo GIS viewer van Cook bekijken.">
         <meta name="copyright" content="Gisarts">
         <meta name="keywords" content="cook, gis, viewer, geo, gisarts"/>
-
+        
+        <link rel="icon" href="./img/favicon.webp">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
         <link href="css/main.css" rel="stylesheet" media="screen">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet"> 
+        <base href="https://demo.gisarts.nl/">
     </head>
     <body>
         <header class="text-center">

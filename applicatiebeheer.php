eb3cece47c7a2a77761c42890ddddc1d7b041e1c
Bono Arts <bonoarts2000@gmail.com>
2023-07-04 14:30:41 +0200

    Add public portal info

--- applicatiebeheer.php
+++ applicatiebeheer.php
@@ -3,6 +3,16 @@
         <h1 class="text-gold text-center">Applicatiebeheer <b>CO</b>OK</h1>
         <p class="text-gold text-center py-3">Richt de applicatie zelf in met het bepalen van functies</p>
         <div class="row">
+            <div class="col-lg-7 py-5">
+                <img src="./img/app3.png" class="img-fluid" alt="app1">
+            </div>
+            <div class="col-lg align-self-center">
+                <h4 class="text-gold m-0">Publieke Informatie</h4>
+                <h2>Volledig beheer middels COOK</h2>
+                <p class="w-75 r-text">Voor zowel intern als extern beheert u volledig de portalen. Tot in de kleinste details kan het naar wens worden aangepast, met als doel de informatie zo effectief mogelijk over te brengen naar de eindgebruiker. Met slechts één klik op de knop kunt u de configuratie publiceren naar de burger, op voorwaarde dat alle AVG-datacontroles in orde zijn.</p>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-lg-7 order-lg-2 py-5">
                 <img src="./img/app1.png" class="img-fluid" alt="app1">
             </div>
@@ -17,9 +27,9 @@
                 <img src="./img/app2.png" class="img-fluid" alt="app1">
             </div>
             <div class="col-lg align-self-center">
-                <h4 class="text-gold m-0">IMPORT EXPORT BESTANDEN</h4>
-                <h2>Voeg zelfstandig bestanden toe van KML tot JSON</h2>
-                <p class="w-75 r-text">In de Geo wereld wordt met veel verschillende bestanden gewerkt, Deze kan de klant zelf toevoegen aan de GIS Viewer van COOK. Andersom is het ook mogelijk om gegevens te exporteren naar Shape en Excel bestanden. Daarnaast is het ook mogelijk om KML en GeoJSon bestanden toe te voegen. Sleep de bestanden in het kaartbeeld en COOK geeft de informatie weer. PDOK heeft een groot aantal datasets downloads beschikbaar. Ook deze kunnen moeiteloos in de COOK GIS Viewer worden toegevoegd.</p>
+                <h4 class="text-gold m-0">Kaart beheer</h4>
+                <h2>Voeg zelfstandig alle OGC kaarten toe</h2>
+                <p class="w-75 r-text">In Nederland zijn er talloze databronnen die op verschillende manieren weergegeven moeten worden. Onze applicatie streeft ernaar om zoveel mogelijk van deze bronnen beschikbaar te maken. Met behulp van ons kaartbeheer kunt u alle bronnen beheren en koppelen aan de gewenste gebruikers.</p>
             </div>
         </div>
     </div>

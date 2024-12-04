e36f121486aadff46a7e88fbea62a45976fa0d83
Bono Arts <b.arts@gisarts.nl>
2024-12-04 10:05:38 +0100

    Add privacy statement

--- footer.php
+++ footer.php
@@ -55,6 +55,20 @@
                                     <p><a href="https://goo.gl/maps/N6KkoTtjPo3DZ9eN8" target="_Blank">Zandkant 11, Sint Anthonis<br>Nederland 5845 EV</a></p>
                                 </div>
                             </div>
+
+                            <div class="row">
+                                <div class="col-sm-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-lock inv" viewBox="0 0 330 330">
+                                    <path d="M65,330h200c8.284,0,15-6.716,15-15V145c0-8.284-6.716-15-15-15h-15V85c0-46.869-38.131-85-85-85
+                                        S80,38.131,80,85v45H65c-8.284,0-15,6.716-15,15v170C50,323.284,56.716,330,65,330z M180,234.986V255c0,8.284-6.716,15-15,15
+                                        s-15-6.716-15-15v-20.014c-6.068-4.565-10-11.824-10-19.986c0-13.785,11.215-25,25-25s25,11.215,25,25
+                                        C190,223.162,186.068,230.421,180,234.986z M110,85c0-30.327,24.673-55,55-55s55,24.673,55,55v45H110V85z"/>
+                                </svg>
+                                </div>
+                                <div class="col-sm-8">
+                                    <p><a href="https://www.gisarts.nl/privacyverklaring" target="_Blank">Privacyverklaring</a></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="col">
